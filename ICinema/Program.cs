aea5cfdba94ab87a7d2a616ab1f5ea5b5679f76b
Yevhen Tkach <zhekatkach04@gmail.com>
2024-05-28 16:40:46 +0300

    IITC-17 feature: keys and connection string loading from .env

--- ICinema/Program.cs
+++ ICinema/Program.cs
@@ -1,35 +1,38 @@
+using DotNetEnv;
 using ICinema.Data;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.EntityFrameworkCore;
 
+Env.Load();
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
-})
-.AddCookie(options =>
-{
-    options.LoginPath = new PathString("/Account/Login");
-})
-.AddGoogle(GoogleDefaults.AuthenticationScheme, options =>
-{
-    IConfigurationSection googleAuthSection = builder.Configuration.GetSection("Google");
-    options.ClientId = googleAuthSection["ClientId"] ?? "";
-    options.ClientSecret = googleAuthSection["ClientSecret"] ?? "";
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+    })
+    .AddCookie(options =>
+    {
+        options.LoginPath = new PathString("/Account/Login");
+    })
+    .AddGoogle(GoogleDefaults.AuthenticationScheme, options =>
+    {
+        options.ClientId = Environment.GetEnvironmentVariable("ClientId") ?? "";
+        options.ClientSecret = Environment.GetEnvironmentVariable("ClientSecret") ?? "";
 
-    options.Scope.Add("profile");
-    options.Scope.Add("email");
-});
+        options.Scope.Add("profile");
+        options.Scope.Add("email");
+    });
 
 builder.Services.AddDistributedMemoryCache();
 
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<CinemaContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ZhenyaServerConnection")));
+builder.Services.AddDbContext<CinemaContext>(options => 
+    options.UseSqlServer(Environment.GetEnvironmentVariable("DbConnectionString")));
 
 var app = builder.Build();
 
