1fc332dff67ee2f77e40672d0befad1577c3b294
olya013 <olha.ermakova@gmail.com>
2024-06-14 19:50:59 +0300

    IITC-34-admin-view-orders
    Added view orders

---
+++ ICinema/ViewModels/OrdersListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using ICinema.Models;
+
+namespace ICinema.ViewModels;
+
+public class OrdersListViewModel
+{
+    public int? Id { get; set; }
+
+    public List<Order> Items { get; set; } = [];
+}
