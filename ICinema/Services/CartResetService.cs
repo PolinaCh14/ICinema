96c5d70865c4e88bd743ce0d1e7a40404e12d068
Yevhen Tkach <zhekatkach04@gmail.com>
2024-06-24 21:21:26 +0300

    feature: autocancel unpaid orders in 1 hour before session start

--- ICinema/Services/CartResetService.cs
+++ ICinema/Services/CartResetService.cs
@@ -1,4 +1,6 @@
 ﻿using ICinema.Data;
+using ICinema.Infrastructure.Constants;
+using Microsoft.EntityFrameworkCore;
 
 namespace ICinema.Services;
 
@@ -23,6 +25,29 @@ public class CartResetService(IServiceScopeFactory serviceScopeFactory) : IHoste
         var ticketsToDelete = bookedTickets.Where(t => DateTime.Now.Subtract(t.CreateDate).TotalMinutes >= 15).ToList();
 
         dbContext.Tickets.RemoveRange(ticketsToDelete);
+        dbContext.SaveChanges();
+
+        var currentDate = DateOnly.FromDateTime(DateTime.Now);
+        var currenTime = TimeOnly.FromDateTime(DateTime.Now);
+        var nowPlusOneHour = TimeOnly.FromDateTime(DateTime.Now.AddHours(1));
+
+
+        var orders = dbContext.Orders
+            .Include(o => o.Tickets).ThenInclude(t => t.Session)
+            .Where(o => (o.OrderStatus == OrderStatuses.NEW || o.OrderStatus == OrderStatuses.PAID)
+                && o.Tickets.First().Session.Date == currentDate)
+            .Select(x => new { Order = x, SessionTime = x.Tickets.First().Session.Time })
+            .ToList();
+
+        foreach (var order in orders)
+        {
+            if (currenTime >= order.SessionTime)
+                order.Order.OrderStatus = OrderStatuses.COMPLETED;
+
+            if (nowPlusOneHour >= order.SessionTime && order.Order.OrderStatus == OrderStatuses.NEW)
+                order.Order.OrderStatus = OrderStatuses.CANCELED;
+        }
+            
         dbContext.SaveChanges();
     }
 
